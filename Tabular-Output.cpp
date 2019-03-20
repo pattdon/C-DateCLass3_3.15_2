be634e3219d68d6f4ccc1529c59f1f5052e1c930
pattadon <35373509+pattdon@users.noreply.github.com>
2019-03-20 17:09:33 +0700

    4.18 Tabular-Output

---
+++ Tabular-Output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int N = 1;
+	cout<<"N"<<"\t10*N"<<"\t100*N"<<"\t1000*N"<<endl;
+   while(N < 6){
+   		cout<<N<<"\t"<<10*N<<"\t"<<100*N<<"\t"<<1000*N<<endl;
+   		N++;
+   }	
+}
